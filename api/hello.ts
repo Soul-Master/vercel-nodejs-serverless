744370ab333d109e032eb6b13cc7db6a9a1ad1c1
Soul_Master <Soul-Master@users.noreply.github.com>
2023-05-03 23:27:06 +0700

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -3,8 +3,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query;
   return res.json({
-    request: {
-      ...req,
+    request: JSON.parse(JSON.stringify(req)),
     }
   })
 }
