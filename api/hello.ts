426d2ee67b3854864dec5ed976143b6bb03993bb
Soul_Master <Soul-Master@users.noreply.github.com>
2023-05-03 23:30:23 +0700

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -3,7 +3,21 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query;
   return res.json({
-    request: JSON.parse(JSON.stringify(req)),
+    request: JSON.parse(JSON.stringify(req, getCircularReplacer())),
     }
   })
+}
+
+function getCircularReplacer() {
+  const seen = new WeakSet();
+  
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
 }
