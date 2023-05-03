43d84c5e66cd485f18abb323b57b4bccb15a2648
Soul_Master <Soul-Master@users.noreply.github.com>
2023-05-03 23:38:30 +0700

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,11 +1,15 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import process from 'node:process';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query;
+  
   return res.json({
     request: JSON.parse(JSON.stringify(req, getCircularReplacer())),
+    version: {
+      ...process.version
     }
-  })
+  });
 }
 
 function getCircularReplacer() {
